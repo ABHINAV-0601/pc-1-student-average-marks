6ceaaa8b96ea673bb35ad3ac375c193b84b5ddde
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-09-02 14:47:49 +0530

    added the Student object in the HashSet

--- src/main/java/com/jap/StudentService.java
+++ src/main/java/com/jap/StudentService.java
@@ -6,13 +6,24 @@ public class StudentService {
 
     public static Set<Student> getAllStudentData(){
         //create six student object by calling Student class parameterized constructor
+        Student s1 = new Student("Abhishek",2,250);
+        Student s2 = new Student("Vaibhav",12,300);
+        Student s3 = new Student("Diwakar",15,275);
+        Student s4 = new Student("Rudra",20,175);
+        Student s5 = new Student("Samantha",22,265);
+        Student s6 = new Student("Salman",25,295);
 
         //create HashSet object and add all the students object inside it
+        Set<Student> studentSet = new HashSet<>();
+        studentSet.add(s1);
+        studentSet.add(s2);
+        studentSet.add(s3);
+        studentSet.add(s4);
+        studentSet.add(s5);
+        studentSet.add(s6);
 
         //return the HashSet object
-
-
-        return null;
+        return studentSet;
     }
 
 
@@ -44,7 +55,8 @@ public class StudentService {
     }
 
     public static void main(String[] args) {
-        Set studentName = getAllStudentData();
+        Set<Student> studentName = getAllStudentData();
+        System.out.println(studentName);
         System.out.println(getAllStudentNameInSorted(studentName));
         System.out.println(calculateAverage(studentName));
     }
